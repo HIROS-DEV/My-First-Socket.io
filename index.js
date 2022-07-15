eb3ed30cda903b2c6106d2f928ecaf67f10993d6
Hiro K <hirosdev@gmail.com>
2022-07-15 16:42:53 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+	socket.on('disconnect', () => {
+		console.log(`User disconnected...`);
+	});
+	socket.on('chat message', (msg) => {
+		io.emit('chat message', msg);
+	});
+});
+
+server.listen(PORT, () => {
+	console.log(`listening on *: ${PORT}`);
+});
